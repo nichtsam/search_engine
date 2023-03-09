a7ecccf8b700e4a66ef20ccd8f8555a0742dd81b
Samuel Jensen <44519206+nichtsam@users.noreply.github.com>
2023-03-09 22:08:20 +0800

    refactor lexer

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,24 @@ impl<'a> Lexer<'a> {
         }
     }
 
+    fn chop(&mut self, length: usize) -> &'a [char] {
+        let token = &self.content[..length];
+        self.content = &self.content[length..];
+        token
+    }
+
+    fn chop_while<P>(&mut self, predicate: P) -> &'a [char]
+    where
+        P: Fn(&char) -> bool,
+    {
+        let mut n = 0;
+        while n < self.content.len() && predicate(&self.content[n]) {
+            n += 1;
+        }
+
+        self.chop(n)
+    }
+
     fn next_token(&mut self) -> Option<&'a [char]> {
         self.trim_left();
 
@@ -31,33 +49,17 @@ impl<'a> Lexer<'a> {
 
         // token starts with number
         if self.content[0].is_numeric() {
-            let mut n = 0;
-            while n < self.content.len() && self.content[n].is_numeric() {
-                n += 1;
-            }
-
-            let token = &self.content[..n];
-            self.content = &self.content[n..];
-
+            let token = self.chop_while(|c| c.is_numeric());
             return Some(token);
         }
         // token starts with alphabet
         if self.content[0].is_alphabetic() {
-            let mut n = 0;
-            while n < self.content.len() && self.content[n].is_alphanumeric() {
-                n += 1;
-            }
-
-            let token = &self.content[..n];
-            self.content = &self.content[n..];
-
+            let token = self.chop_while(|c| c.is_alphanumeric());
             return Some(token);
         }
 
         // token starts with symbols
-        let token = &self.content[..1];
-        self.content = &self.content[1..];
-
+        let token = self.chop(1);
         Some(token)
     }
 }
